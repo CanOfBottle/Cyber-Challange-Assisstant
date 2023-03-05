06a0c168d534b6828bdc8f3ef3418bcdb533d781
Dale Marriott <109925741+CanOfBottle@users.noreply.github.com>
2023-03-05 17:32:50 -0400

    Added another tool to both versions. Changed wording on a couple strings as well

--- CCA If-Else Version.py
+++ CCA If-Else Version.py
@@ -5,7 +5,7 @@ answer = "y" #starts with answer already set to y in order to start the loop
 
 while answer == "y":
 
-    print("Welcome to the Cyber Challange Assistant! Please select which option you would like! \n \n 1 - Base64 Decode \n \n 2 - ROT13 Decryptor \n \n 3 - ASCII Code Converter \n ")
+    print("Welcome to the Cyber Challange Assistant! Please select which option number you would like! \n \n 1 - Base64 Decode \n \n 2 - ROT13 Decryptor \n \n 3 - ASCII Code Converter \n \n 4 - Defang IP Address \n ")
 
 
     decoder = input("Which option would you like? ") #must type choice exactly as it is printed
@@ -17,10 +17,10 @@ while answer == "y":
         print("\n")
         decodedString = base64.b64decode(codedString)
         print("your decoded string:" + " " + codecs.decode(decodedString) + "\n")
-        print("would you like to decode something else?\n")
+        print("would you like to use something else?\n")
         loopAnswer = input("Please answer with y/n: ")
         print("\n")
-        if loopAnswer == "y":
+        if loopAnswer == "y": #only checks that it is spelled exactly the same as printed above
             continue
         else:
                 exit()
@@ -30,7 +30,7 @@ while answer == "y":
         rot13 = codecs.encode(text, 'rot_13')
         print("your decoded string:" + " " + rot13 + "\n")
         text = codecs.decode(rot13, 'rot_13')
-        print("would you like to decode something else?\n")
+        print("would you like to use something else?\n")
         loopAnswer = input("Please answer with y/n: ")
         print("\n")
         if loopAnswer == "y": #only checks that you used y or n. Anything other than y will close it
@@ -38,10 +38,30 @@ while answer == "y":
         else:
             exit()
     elif decoder == "3": #use commas to seperate the codes. No spaces. 
-        asciiCodes = input ("What ASCII codes need to be converted? ")
+        asciiCodes = input ("What ASCII codes need to be converted? (please seperate each code with a comma): ")
         asciiList = asciiCodes.split (",")
         asciiListInt = [int (x) for x in asciiList]
         print(''.join(chr(number) for number in asciiListInt))
+        print("would you like to use something else?\n")
+        loopAnswer = input("Please answer with y/n: ")
+        print("\n")
+        if loopAnswer == "y": #only checks that you used y or n. Anything other than y will close it
+            continue
+        else:
+            exit()
+    elif decoder == "4":
+        ip_address = input("Please enter the IP Address that you need to defang: ")
+        split_address = ip_address.split(".")
+        defang = "[.]"
+        defanged_address = defang.join(split_address)
+        print("Here is your defanged IP Address:" + " " + defanged_address)
+        print("would you like to use something else?\n")
+        loopAnswer = input("Please answer with y/n: ")
+        print("\n")
+        if loopAnswer == "y": #only checks that you used y or n. Anything other than y will close it
+            continue
+        else:
+            exit()
     else:
         print("Sorry, I only have the three options at this time.")
         quit()
--- Cyber Challange Assistant.py
+++ Cyber Challange Assistant.py
@@ -5,7 +5,7 @@ answer = "y" #starts with answer already set to y in order to start the loop
 
 while answer == "y":
 
-    print("Welcome to the Cyber Challange Assistant! Please select which option you would like! \n \n 1 - Base64 Decoder \n \n 2 - ROT13 Decryptor \n \n 3 - ASCII Code Converter \n ")
+    print("Welcome to the Cyber Challange Assistant! Please select which option number you would like! \n \n 1 - Base64 Decoder \n \n 2 - ROT13 Decryptor \n \n 3 - ASCII Code Converter \n\n 4 - Defang IP Address \n ")
 
 
     decoder = input("Which option would you like? ") #must type choice exactly as it is printed
@@ -17,10 +17,10 @@ while answer == "y":
             print("\n")
             decodedString = base64.b64decode(codedString)
             print("your decoded string:" + " " + codecs.decode(decodedString) + "\n")
-            print("would you like to decode something else?\n")
+            print("would you like to use something else?\n")
             loopAnswer = input("Please answer with y/n: ")
             print("\n")
-            if loopAnswer == "y":
+            if loopAnswer == "y": #only checks that you used y or n. Anything other than y will close it
                 continue
             else:
                 exit()
@@ -29,8 +29,7 @@ while answer == "y":
             print("\n")
             rot13 = codecs.encode(text, 'rot_13')
             print("your decoded string:" + " " + rot13 + "\n")
-            text = codecs.decode(rot13, 'rot_13')
-            print("would you like to decode something else?\n")
+            print("would you like to use something else?\n")
             loopAnswer = input("Please answer with y/n: ")
             print("\n")
             if loopAnswer == "y": #only checks that you used y or n. Anything other than y will close it
@@ -38,10 +37,30 @@ while answer == "y":
             else:
                 exit()
         case "3":
-            asciiCodes = input ("What ASCII codes need to be converted? ")#use commas to seperate the codes
+            asciiCodes = input ("What ASCII codes need to be converted?(please seperate each code with a comma): ")#use commas to seperate the codes
             asciiList = asciiCodes.split (",")
             asciiListInt = [int (x) for x in asciiList]
             print(''.join(chr(number) for number in asciiListInt))
+            print("would you like to use something else?\n")
+            loopAnswer = input("Please answer with y/n: ")
+            print("\n")
+            if loopAnswer == "y": #only checks that you used y or n. Anything other than y will close it
+                continue
+            else:
+                exit()
+        case "4":
+            ip_address = input("Please enter the IP Address that you need to defang: ")
+            split_address = ip_address.split(".")
+            defang = "[.]"
+            defanged_address = defang.join(split_address)
+            print("Here is your defanged IP Address:" + " " + defanged_address)
+            print("would you like to use something else?\n")
+            loopAnswer = input("Please answer with y/n: ")
+            print("\n")
+            if loopAnswer == "y": #only checks that you used y or n. Anything other than y will close it
+                continue
+            else:
+                exit()
         case _:
-            print("Sorry, I only have the three options at this time.")
+            print("Sorry, I only have what is listed.")
             quit()
